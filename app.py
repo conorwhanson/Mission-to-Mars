42ff8ad9a67751b28a52ca973ca39c523a006967
Conor Hanson <condorhanson@gmail.com>
2022-05-29 16:35:41 -0500

    App built

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ app.config["MONGO_URI"] = 'mongodb://localhost:27017/mars_app'
 mongo = PyMongo(app)
 
 ## define app route to root page
-@app.rout('/')
+@app.route("/")
 def index():
     mars = mongo.db.mars.find_one()
     return render_template('index.html', mars=mars)
 
-@app.route('scrape')
+@app.route("/scrape")
 def scrape():
     mars = mongo.db.mars ## define variable that points to mongo db
     mars_data = scraping.scrape_all() # holds scraped data
@@ -22,4 +22,4 @@ def scrape():
     return redirect('/', code=302)
 
 if __name__ == "__main_":
-    app.run()
+    app.run(debug=True)
---
+++ scraping.py
@@ -0,0 +1,105 @@
+
+# Dependencies
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+import numpy as np
+import requests
+import datetime as dt
+
+
+def scrape_all():
+    # set path
+    exec_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **exec_path, headless=True)
+
+    news_title, news_paragraph = mars_news(browser)
+
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_img": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+
+    }
+    browser.quit()
+    return data
+
+def mars_news(browser):
+    # ## Mars News
+    ##########################
+    # visit the mars NASA site
+    url = 'https://redplanetscience.com'
+    browser.visit(url)
+
+    # Optional delay for loading the page
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
+
+    # convert browser html to soup object
+    html = browser.html
+    red_soup = soup(html, 'html.parser')
+
+    try:
+        slide_elem = red_soup.select_one('div.list_text')
+
+        # find Mars news title
+        news_title = slide_elem.find('div', class_='content_title').get_text()
+
+        # find Mars news summary
+        summary = slide_elem.find('div', class_='article_teaser_body').get_text()
+
+    except AttributeError:
+        return None, None
+
+    return news_title, summary
+
+def featured_image(browser):
+    # ## Image
+    ###########################
+    # Scrape the featured image
+    
+    # Visit URL
+    url = 'https://spaceimages-mars.com'
+    browser.visit(url)
+
+    
+    # find the button for featured image and click it
+    full_feature_img = browser.find_by_tag('button')[1]
+    full_feature_img.click()
+
+    # set up html parser
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
+
+    try:
+        # Find the relative image url
+        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+    except AttributeError:
+        return None
+
+    # url above is only partial; need to add it onto the base url and save it as a variable
+    img_url = f"https://spaceimages-mars.com/{img_url_rel}"
+
+    return img_url
+
+def mars_facts():
+    # ## Mars Facts
+    ############################
+    try:
+        # read in facts about Mars with pd.read_html
+        df = pd.read_html('https://galaxyfacts-mars.com/')[0]
+
+    except BaseException:
+        return None
+
+    df.columns = ('Description','Mars','Earth')
+    df.set_index('Description', inplace=True)
+
+    # turn the pulled table back into HTML for coding
+    return df.to_html()
+
+if __name__ == "__main__":
+    # if it's running as a script, then print the scraped data
+    print(scrape_all())
